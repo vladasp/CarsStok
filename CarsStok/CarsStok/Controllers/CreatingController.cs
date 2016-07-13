ba2fd4d3593ac5fb2715bba0dfeb48f241bc8759
vladasp <vladalogin@ukr.net>
2016-07-13 04:55:21 +0300

    Added project

---
+++ CarsStok/CarsStok/Controllers/CreatingController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarsStok.Controllers
+{
+    public class CreatingController : Controller
+    {
+        Models.StokDBEntities context = new Models.StokDBEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Update(int id)
+        {
+            var list = context.RentalHistories.Where(i => i.Id == id).First();
+            return View("Update", list);
+        }
+
+        public ActionResult UpdateComplete(List<string> Data)
+        {
+            string name = Data[0];
+            string model = Data[1];
+            int temp;
+            int period = (!int.TryParse(Data[2], out temp)) ? 0 : temp;
+            int tempId;
+            int ID = (!int.TryParse(Data[3], out tempId)) ? 0 : tempId;
+
+            var car = context.Cars.Where(i => i.Cars_model == model);
+
+            if (car.Count() == 0)
+            {
+                context.Cars.Add(new Models.Car
+                {
+                    Cars_model = model
+                });
+                context.SaveChanges();
+                car = context.Cars.Where(i => i.Cars_model == model);
+            }
+
+            var item = context.RentalHistories.Where(i => i.Id == ID).First();
+
+            item.Id = ID;
+            item.Client_name = name;
+            item.Cars_model = model;
+            item.Rental_period = period;
+            item.Cars_id = car.First().Id;
+
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (DbEntityValidationException dbEx)
+            {
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        System.Console.WriteLine("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return RedirectToAction("Index", "Home", context);
+        }
+
+        [HttpPost]
+        public ActionResult Insert(List<string> Data)
+        {
+            string name = Data[0];
+            string model = Data[1];
+            int temp;
+            int period = (!int.TryParse(Data[2], out temp)) ? 0 : temp;
+
+            var car = context.Cars.Where(i => i.Cars_model == model);
+
+            if (car.Count() == 0)
+            {
+                context.Cars.Add(new Models.Car
+                {
+                    Cars_model = model
+                });
+                context.SaveChanges();
+            }
+
+            int id = context.Cars.Where(i => i.Cars_model == model).First().Id;
+
+            var item = new Models.RentalHistory
+            {
+                Client_name = name,
+                Cars_id = id,
+                Cars_model = model,
+                Rental_period = period
+            };
+            try
+            {
+                context.RentalHistories.Add(item);
+                context.SaveChanges();
+            }
+            catch
+            {
+            }
+            return RedirectToAction("Index", "Home", context);
+        }
+
+    }
+}
---
+++ CarsStok/CarsStok/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarsStok.Controllers
+{
+    public class HomeController : Controller
+    {
+        Models.StokDBEntities context = new Models.StokDBEntities();
+        IList<Models.RentalHistory> rentals;
+
+        public ActionResult Index()
+        {
+            if (context.RentalHistories.ToList() == null) AddEntries();
+
+            var list = (from item in context.RentalHistories
+                        select new
+                        {
+                            Id = item.Id,
+                            Client_name = item.Client_name,
+                            Cars_id = item.Cars_id,
+                            Cars_model =item.Cars_model,
+                            Rental_period = item.Rental_period
+                        }).ToList();
+            return View(list);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var item = context.RentalHistories.Where(x => x.Id == id).First();
+                context.RentalHistories.Remove(item);
+                context.SaveChanges();
+            }
+            catch
+            {
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Select(string name)
+        {
+            var list = (from item in context.RentalHistories
+                         where item.Client_name == name
+                         select item
+                         ).ToList();
+
+            return View("Index", list);
+        }
+
+        private void AddEntries()
+        {
+            rentals = new List<Models.RentalHistory>();
+            rentals.Add(new Models.RentalHistory
+            {
+                Client_name = "Vlad",
+                Cars_id = 1,
+                Cars_model = "Zaz",
+                Rental_period = 10000
+            });
+            try
+            {
+                context.RentalHistories.AddRange(rentals);
+                context.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
